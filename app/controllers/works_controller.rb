f1d5eace6f97f1261f2e99fa561e6c15b5984e31
komadorihouse <komadorihouse@yahoo.co.jp>
2021-07-13 12:48:00 +0900

    各ページのルーティングを実装

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,22 @@
 class WorksController < ApplicationController
   def index
+    @info = "News"
   end
+
+  def works
+    @info = "Works"
+  end
+
+  def profile
+    @info = "Profile"
+  end
+
+  def contact
+    @info = "Contact"
+  end
+  
+  def link
+    @info = "Link"
+  end
+  
 end
