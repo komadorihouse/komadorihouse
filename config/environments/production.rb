3064b92d6f9f6d96eeb4cf881230d4d9b960822b
komadorihouse <komadorihouse@yahoo.co.jp>
2021-08-06 16:08:30 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
     port:                  587,
     domain:               'gmail.com',
     user_name:            ENV["ACTION_MAILER_USER"],
-    password:             "vngcwhchlvkdybcf",
+    password:             ENV["ACTION_MAILER_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto:  true
    }
