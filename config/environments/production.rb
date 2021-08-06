de90be15b9d73f4bad9e5230161014c56b1ac11d
komadorihouse <komadorihouse@yahoo.co.jp>
2021-08-06 15:58:51 +0900

    comitt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,16 @@
 Rails.application.configure do
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = "komadorihouse.herokuapp.com"
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV["ACTION_MAILER_USER"],
+    password:             "vngcwhchlvkdybcf",
+    authentication:       'plain',
+    enable_starttls_auto:  true
+   }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
