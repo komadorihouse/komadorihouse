01109ba7f5b03bf85361039cf910e313047e6e16
komadorihouse <komadorihouse@yahoo.co.jp>
2021-08-06 15:22:26 +0900

    メール設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
     port:                  587,
     domain:               'gmail.com',
     user_name:            ENV["ACTION_MAILER_USER"],
-    password:             'jagogzetlccqqnlp',
+    password:             "vngcwhchlvkdybcf",
     authentication:       'plain',
     enable_starttls_auto:  true
    }
