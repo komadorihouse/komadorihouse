002531a1dc56927e08ff998b3e3076139f7a286d
komadorihouse <komadorihouse@yahoo.co.jp>
2021-12-18 07:12:56 +0900

    アーティスト別検索後showから戻ってもアーティスト検索が適用されている機能の実装

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -59,13 +59,26 @@ class WorksController < ApplicationController
   end
 
   def works
-    @type = Type.all
-    @creaters = Creater.all
-    @info = "Works"
-    @workslist = Work.page(params[:index]).per(21).where("artist_id != 3 and artist_id != 8").order('created_year DESC')
-    @works = Work.page(params[:contents]).per(21).where("artist_id != 3 and artist_id != 8").order('created_year DESC')
-    @body_info = "All Works"
-    if request.xhr?
+    if params.has_key?(:back) #atist別でshowからもどるとき
+      @creater_id = params[:back].to_i
+      @info = Creater.find(@creater_id).name
+      @workslist = Work.page(params[:index]).per(21).where("artist_id != 3 and artist_id != 8").order('created_year DESC')
+      @works = Work.page(params[:contents]).per(21).where("artist_id = #{@creater_id}" ).order('created_year DESC')
+      @body_info = "#{ @info } Works"
+      @creaters = Creater.all
+      @type = Type.all
+      @artist_search = 1
+    else #通常の遷移
+      @type = Type.all
+      @creaters = Creater.all
+      @info = "Works"
+      @workslist = Work.page(params[:index]).per(21).where("artist_id != 3 and artist_id != 8").order('created_year DESC')
+      @works = Work.page(params[:contents]).per(21).where("artist_id != 3 and artist_id != 8").order('created_year DESC')
+      @body_info = "All Works"
+      @artist_search = 0
+    end
+
+    if request.xhr? #非同期通信時
       if params.has_key?(:index)
         render "lists"
       elsif params.has_key?(:contents)
@@ -88,7 +101,8 @@ class WorksController < ApplicationController
     @works = Work.where(artist_id: params[:id]).page(params[:contents]).per(21).order('created_year DESC')
     @body_info = "#{@info} Works"
     @creater_id = params[:id]
-    binding.pry
+    @artist_search = 1
+
     if params.has_key?(:contents)
       render 'contents'
     else
@@ -118,6 +132,14 @@ class WorksController < ApplicationController
     @work = Work.find(params[:id]).image
     @creater = Creater.find(@item.artist_id).name
     @info = @item.title
+    serch = params[:serch].to_i
+
+    if serch == 1
+      @artist_search = 1
+    else
+      @artist_search = 0
+    end
+
     if request.xhr?
       if params.has_key?(:index)
         render "lists"
